c996190437879a20a163fa3ba0905643c9a63573
Charlie Jeffries-Tipton <charlie.jeffries-tipton@SVG.local>
2020-10-20 01:01:12 +0100

    Make amendments to slide in animations on scroll

--- src/Components/Home/Projects/Projects.js
+++ src/Components/Home/Projects/Projects.js
@@ -2,21 +2,24 @@ import React from 'react';
 import classes from './Projects.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import TabNavigate from './TabNavigate/TabNavigate';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 const Projects = () => {
     return (
         <Container className={classes.Projects}>
-            <Row className="my-4">
-                <Col>
-                    <h1 className={[classes.HeadingOne, "mb-0"].join(' ')}>Some of our</h1>
-                    <h1 className={[classes.HeadingTwo, "mb-0"].join(' ')}>Recent Projects</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <TabNavigate/>
-                </Col>
-            </Row>
+            <ScrollAnimation animateIn="fadeInLeft" duration={1}>
+                <Row className="my-4">
+                    <Col>
+                        <h1 className={[classes.HeadingOne, "mb-0"].join(' ')}>Some of our</h1>
+                        <h1 className={[classes.HeadingTwo, "mb-0"].join(' ')}>Recent Projects</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <TabNavigate/>
+                    </Col>
+                </Row>
+            </ScrollAnimation>
         </Container>
     )
 }
--- src/Components/Home/WeUnleash/WeUnleash.js
+++ src/Components/Home/WeUnleash/WeUnleash.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import SpanielImage from '../../../media/images/Spaniel01_gradient.png';
 import classes from './WeUnleash.css';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 const WeUnleash = () => {
     return (
         <Container fluid={true} className={classes.WeUnleash}>
             <Row>
                 <Col className={classes.UnleashColumn} xs={12} lg={6}>
-                    <div>
-                        <p className={[classes.UnleashPoints, "mb-4"].join(' ')}>Brand, Dev, Ecom, Marketing</p>
-                        <h1 className={[classes.HeadingOne, "mb-0"].join(' ')}>We unleash</h1>
-                        <h1 className={[classes.HeadingTwo, "mb-0"].join(' ')}>business potential</h1>
-                        <p className={[classes.UnleashText, "my-4"].join(' ')}>
-                            We create brand experiences which are memorable and distinct. Our experienced team create amd develop
-                            brands with personality and resonance.
-                        </p>
-                        <p className={classes.LetsTalk}>Let's talk</p>
-                    </div>
+                    <ScrollAnimation animateIn="fadeInLeft" duration={1}>
+                        <div>
+                            <p className={[classes.UnleashPoints, "mb-4"].join(' ')}>Brand, Dev, Ecom, Marketing</p>
+                            <h1 className={[classes.HeadingOne, "mb-0"].join(' ')}>We unleash</h1>
+                            <h1 className={[classes.HeadingTwo, "mb-0"].join(' ')}>business potential</h1>
+                            <p className={[classes.UnleashText, "my-4"].join(' ')}>
+                                We create brand experiences which are memorable and distinct. Our experienced team create amd develop
+                                brands with personality and resonance.
+                            </p>
+                            <p className={classes.LetsTalk}>Let's talk</p>
+                        </div>
+                    </ScrollAnimation>
                 </Col>
                 <Col xs={12} lg={6}>
                     <img src={SpanielImage} className="w-100" alt="Running Spaniel"/>
